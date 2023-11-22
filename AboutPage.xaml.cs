d7023c2eb21dab97b7acf86325adc006462005e3
IRIS-GENȚIANA MUNTEAN <iris.muntean@stud.ubbcluj.ro>
2023-11-22 21:41:34 +0200

    Add project files.

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Muntean_Iris_Lab7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Muntean_Iris_Lab7;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
