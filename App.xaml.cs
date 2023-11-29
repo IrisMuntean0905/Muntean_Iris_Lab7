5134cb3724fd9e070d3be596acb8f8f127035e8d
IRIS-GENȚIANA MUNTEAN <iris.muntean@stud.ubbcluj.ro>
2023-11-29 23:03:04 +0200

    lab8 rezolvare

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,8 +1,28 @@
-﻿namespace Muntean_Iris_Lab7;
+﻿using System;
+using Muntean_Iris_Lab7.Data;
+using System.IO;
+
+
+namespace Muntean_Iris_Lab7;
 
 public partial class App : Application
 {
-	public App()
+    static ShoppingListDatabase database;
+    public static ShoppingListDatabase Database
+    {
+        get
+        {
+            if (database == null)
+            {
+                database = new
+               ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+               LocalApplicationData), "ShoppingList.db3"));
+            }
+            return database;
+        }
+    }
+
+    public App()
 	{
 		InitializeComponent();
 
